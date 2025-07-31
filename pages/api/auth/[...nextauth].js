b8ed18270876b6bfa43b29270f0d0e5d0726cc47
Nilay Barde <nilaybarde@gmail.com>
2025-07-30 20:41:29 -0400

    add debug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,6 +10,7 @@ export default NextAuth({
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
+    debug: true,
     callbacks: {
         async session({ session }) {
             if (session?.user?.email) {
